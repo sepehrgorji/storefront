13d2b682f248f485c18df1654bd502cfd9c063ca
sepehr gorji <freshsgr@gmail.com>
2025-11-28 22:20:36 +0330

    Update views.py

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product,Customer
 
 
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    query_set = Customer.objects.filter(email__endswith=".com")
