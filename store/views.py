e133f0a3cbaab9ee654b066e2250a3558b2990b4
freshsgr@gmail.com <freshsgr@gmail.com>
2025-12-17 20:55:32 +0330

    Creating custom serializer field

--- store/views.py
+++ store/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializers
 # Create your views here.
 
 @api_view()
 def product_list(request):
-    return Response("ok")
-
+    queryset = Product.objects.all()
+    serializer = ProductSerializers(queryset, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, pk=id)
+    serailzer = ProductSerializers(product)
+    return Response(serailzer.data)
